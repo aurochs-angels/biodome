530662bd7d98f21bcae7a364758320d3bd10853f
Aino Nummikallio <aino.nummikallio@gmail.com>
2017-11-25 14:01:57 +0200

    Changed sensor files to reflect temp sensor

---
+++ honeywell.h
@@ -0,0 +1,9 @@
+
+#include "string.h"
+class honeywell {
+public:
+  honeywell(/*i2c inits*/);
+  ~honeywell();
+
+private:
+};
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "gpioInit.h"
+int main() {
+  gpioInit one = gpioInit();
+  gpioInit two = gpioInit("6");
+}
