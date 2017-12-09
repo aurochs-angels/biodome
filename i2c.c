c8139907edc4aca39dd494cc5211fa18f3c35073
Aino Nummikallio <aino.nummikallio@gmail.com>
2017-12-09 13:45:15 +0200

    Removed code for old sensor, started code for new sensor

--- i2c.c
+++ i2c.c
@@ -16,6 +16,13 @@ int main() {
   double humidity;
   double celsius;
   double percents;
+#define TEMP_MEASURE_HOLD 0xE3
+#define HUM_MEASURE_HOLD 0xE5
+#define TEMP_MEASURE_NOHOLD 0xF3
+#define HUM_MEASURE_NOHOLD 0xF5
+#define WRITE_USER_REG 0xE6
+#define READ_USER_REG 0xE7
+#define SOFT_RESET 0xFE
 
   snprintf(filename, 19, "/dev/i2c-%d", adapter);
   file = open(filename, O_RDWR);
@@ -32,53 +39,8 @@ int main() {
       if (read(file, buffer, 4) != 4) {
         printf("Four bytes not read.\n");
         return -3;
-      } else {
-        unsigned int bits_0[8];
-        unsigned int bits_1[8];
-        unsigned int bits_2[8];
-        unsigned int bits_3[8];
-        unsigned int temp_bits[14];
-        unsigned int humidity_bits[14];
-
-        printf("Bytes returned: %03x\n, %03x\n, %03x\n, %03x\n", buffer[0],
-               buffer[1], buffer[2], buffer[3]);
-        for (int i = 0; i < 8; i++) {
-          bits_0[i] = (buffer[0] & (1 << i)) != 0;
-          bits_1[i] = (buffer[1] & (1 << i)) != 0;
-          bits_2[i] = (buffer[2] & (1 << i)) != 0;
-          bits_3[i] = (buffer[3] & (1 << i)) != 0;
-        }
-        for (int n = 0; n < 8; n++) {
-          humidity_bits[n] = bits_0[n + 2];
-        }
-        for (int m = 0; m < 8; m++) {
-          humidity_bits[6 + m] = bits_1[m];
-        }
-        for (int j = 0; j < 8; j++) {
-          temp_bits[j] = bits_2[j];
-        }
-        for (int k = 0; k < 6; k++) {
-          temp_bits[8 + k] = bits_3[k];
-        }
-        temperature = buffer_to_double(temp_bits);
-        humidity = buffer_to_double(humidity_bits);
-        celsius = 125 * (temperature / 16382);
-        percents = 100 * (humidity / 16382);
-
-        printf("Temperature %f\n Humidity %f percent.", celsius, percents);
-
-        return 1;
       }
+      return 1;
     }
   }
-}
-double buffer_to_double(unsigned int buff[14]) {
-  double result = 0;
-  for (int i = 0; i < 14; i++) {
-    if (buff[i] == 1) {
-      result += pow(2, i);
-    }
-  }
-
-  return result;
 }
